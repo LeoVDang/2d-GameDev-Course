bb79a6ab282a75c42678d48fd326f8d71d60eee7
LeoVDang <161635420+LeoVDang@users.noreply.github.com>
2024-04-17 11:02:35 -0700

    M5 L7 Spawn Items Randomly

--- 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
+++ 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var collectible_scenes := [
+	preload("coin.tscn"),
+	preload("energy_pack.tscn")
 ]
+
 
 
 func _ready() -> void:
@@ -9,4 +12,11 @@ func _ready() -> void:
 
 
 func _on_timer_timeout() -> void:
-	pass
+	var viewport_size := get_viewport_rect().size
+	var random_position := Vector2(0, 0)
+	
+	
+	var random_collectible_scene: PackedScene = collectible_scenes.pick_random()
+	var collectible_instance := random_collectible_scene.instantiate()
+	add_child(collectible_instance)
+	collectible_instance.position = random_position
