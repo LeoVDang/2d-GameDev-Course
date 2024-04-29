8e2ccef10290dfb13b62e6fc474237d5afa1b0c5
LeoVDang <161635420+LeoVDang@users.noreply.github.com>
2024-04-29 14:20:04 -0700

    M6 L3 The outline thickens

--- M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
+++ M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+
 
 func _ready() -> void:
-	pass
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	queue_free()
+	
 
-
+	
 func _on_mouse_entered() -> void:
-	pass
+	var tween := create_tween()
+
+
+
+func _on_mouse_exited():
+	var tween := create_tween()
+
