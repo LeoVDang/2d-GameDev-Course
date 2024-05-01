c962398a4eee51b19e74c089f7625eada1bdaf15
LeoVDang <161635420+LeoVDang@users.noreply.github.com>
2024-05-01 10:22:44 -0700

    M6 L5 Open the chest

--- M06.looting_workbook/lessons/chest.gd
+++ M06.looting_workbook/lessons/chest.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -20,3 +20,22 @@ func _on_mouse_exited():
 
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _input_event(viewport: Node, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	
+	var is_open = false
+	
+	if event_is_mouse_click:
+		if is_open:
+			return
+		is_open = true
+		input_pickable = false
+		open()
+
+func open() -> void:
+	animation_player.play("open")
