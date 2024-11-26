59ebdef5b573382c83714cc532d41892d951f2e1
LeoVDang <dangleo730@gmail.com>
2024-11-26 09:24:22 -0800

    M7 L10 Talking with emotion

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -3,6 +3,14 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+
+var expressions := {
+"happy": preload("res://assets/emotion_happy.png"),
+"regular": preload("res://assets/emotion_regular.png"),
+"sad": preload("res://assets/emotion_sad.png"),
+}
 
 var current_item_index := 0
 
@@ -14,7 +22,8 @@ var letter: String = letters_list[0]
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
 	var text_appearing_duration := 1.2
@@ -35,9 +44,25 @@ func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 
-var dialogue_items: Array[String] = [
-	"Roses are red",
-	"Violets are blue",
-	"i'm learning about Arrays",
-	"And so are you!",
+var dialogue_items: Array[Dictionary] = [
+{
+			"expression": expressions["regular"],
+			"text":"I'm learning about Arrays..."
+},
+{
+			"expression": expressions["sad"],
+			"text": "... and it is a little bit complicated."
+},
+{
+			"expression": expressions["happy"],
+			"text": "Let's see if I got it right: an array is a list of values!",
+},
+{
+			"expression": expressions["regular"],
+			"text": "Did I get it right? Did I?",
+},
+{
+			"expression": expressions["happy"],
+			"text": "Hehe! Bye Bye~!",
+}
 ]
