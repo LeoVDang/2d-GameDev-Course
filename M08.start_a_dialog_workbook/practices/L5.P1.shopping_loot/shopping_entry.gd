830317b3bd7a4f7ca96ca53bf85f5e07fd872f9f
LeoVDang <dangleo730@gmail.com>
2024-12-18 16:49:00 -0800

    M8 L5 Stronger guarantees

--- M08.start_a_dialog_workbook/practices/L5.P1.shopping_loot/shopping_entry.gd
+++ M08.start_a_dialog_workbook/practices/L5.P1.shopping_loot/shopping_entry.gd
@@ -1,7 +1,9 @@
 class_name ShoppingEntry extends Resource
 
 # You need to export three properties:
-#
+@export var text := ""
+@export var amount := 1
+@export var price := 10
 # 1. text: a string that represents the name of the item
 # 2. amount: an integer that represents the amount of the item
 # 3. price: an integer that represents the price of the item
